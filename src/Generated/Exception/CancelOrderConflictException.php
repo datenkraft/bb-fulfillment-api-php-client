4f6f084de6cb28c6aa754242c1460a3d645a49eb
Raphael Pansi <raphael.pansi@datenkraft.com>
2022-07-07 14:24:27 +0200

    BB-2907 Update packages and regenerate client

--- src/Generated/Exception/CancelOrderConflictException.php
+++ src/Generated/Exception/CancelOrderConflictException.php
@@ -4,6 +4,9 @@ namespace Datenkraft\Backbone\Client\FulfillmentApi\Generated\Exception;
 
 class CancelOrderConflictException extends ConflictException
 {
+    /**
+     * @var \Datenkraft\Backbone\Client\FulfillmentApi\Generated\Model\ErrorResponse
+     */
     private $errorResponse;
     public function __construct(\Datenkraft\Backbone\Client\FulfillmentApi\Generated\Model\ErrorResponse $errorResponse)
     {
@@ -12,10 +15,10 @@ class CancelOrderConflictException extends ConflictException
 Available message codes:
 - ORDER_NOT_CANCELABLE: The order could not be canceled anymore
 - ORDER_ALREADY_CANCELED: The order is already canceled
-- ORDER_CANCELLATION_ALREADY_EXISTS: An order cancellation request already exists, which needs manual approval', 409);
+- ORDER_CANCELLATION_ALREADY_EXISTS: An order cancellation request already exists, which needs manual approval');
         $this->errorResponse = $errorResponse;
     }
-    public function getErrorResponse()
+    public function getErrorResponse() : \Datenkraft\Backbone\Client\FulfillmentApi\Generated\Model\ErrorResponse
     {
         return $this->errorResponse;
     }
--- src/Generated/Exception/PostAuthRoleIdentityCollectionUnprocessableEntityException.php
+++ src/Generated/Exception/PostAuthRoleIdentityCollectionUnprocessableEntityException.php
@@ -4,13 +4,16 @@ namespace Datenkraft\Backbone\Client\FulfillmentApi\Generated\Exception;
 
 class PostAuthRoleIdentityCollectionUnprocessableEntityException extends UnprocessableEntityException
 {
+    /**
+     * @var \Datenkraft\Backbone\Client\FulfillmentApi\Generated\Model\ErrorResponse
+     */
     private $errorResponse;
     public function __construct(\Datenkraft\Backbone\Client\FulfillmentApi\Generated\Model\ErrorResponse $errorResponse)
     {
-        parent::__construct('Unprocessable Entity', 422);
+        parent::__construct('Unprocessable Entity');
         $this->errorResponse = $errorResponse;
     }
-    public function getErrorResponse()
+    public function getErrorResponse() : \Datenkraft\Backbone\Client\FulfillmentApi\Generated\Model\ErrorResponse
     {
         return $this->errorResponse;
     }
