ee2f8f941cf56a1a808984bd42847363ea81cd63
CircleCI <office@datenkraft.com>
2023-05-16 10:00:39 +0000

    Autogenerated client update [publish]

--- src/Generated/Endpoint/GetProductJournalCollection.php
+++ src/Generated/Endpoint/GetProductJournalCollection.php
@@ -12,6 +12,9 @@ class GetProductJournalCollection extends \Datenkraft\Backbone\Client\Fulfillmen
     * @param array $queryParameters {
     *     @var int $page The page to read. Default is the first page.
     *     @var int $pageSize The maximum size per page is 100. Default is 100.
+    *     @var string $paginationMode The paginationMode to use:
+    - default: The total number of items in the collection will not be calculated.
+    - totalCount: The total number of items in the collection will be calculated. This can mean loss of performance.
     *     @var string $shopCode The shopCode used internally to distinguish between clients.\
     _This code is optional, if your identity is assigned to only one shop.
     Otherwise the response would be a 422 HTTP Error._
@@ -54,11 +57,12 @@ class GetProductJournalCollection extends \Datenkraft\Backbone\Client\Fulfillmen
     protected function getQueryOptionsResolver() : \Symfony\Component\OptionsResolver\OptionsResolver
     {
         $optionsResolver = parent::getQueryOptionsResolver();
-        $optionsResolver->setDefined(array('page', 'pageSize', 'shopCode', 'filter[dateFrom]', 'filter[dateTo]', 'filter[reason]'));
+        $optionsResolver->setDefined(array('page', 'pageSize', 'paginationMode', 'shopCode', 'filter[dateFrom]', 'filter[dateTo]', 'filter[reason]'));
         $optionsResolver->setRequired(array());
-        $optionsResolver->setDefaults(array());
+        $optionsResolver->setDefaults(array('paginationMode' => 'default'));
         $optionsResolver->setAllowedTypes('page', array('int'));
         $optionsResolver->setAllowedTypes('pageSize', array('int'));
+        $optionsResolver->setAllowedTypes('paginationMode', array('string'));
         $optionsResolver->setAllowedTypes('shopCode', array('string'));
         $optionsResolver->setAllowedTypes('filter[dateFrom]', array('string'));
         $optionsResolver->setAllowedTypes('filter[dateTo]', array('string'));
