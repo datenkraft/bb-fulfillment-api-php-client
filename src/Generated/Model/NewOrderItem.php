3763d62a941943a1ab2863308c55f2e7f0643711
CircleCI <office@datenkraft.com>
2023-05-19 09:36:09 +0000

    Autogenerated client update [publish]

--- src/Generated/Model/NewOrderItem.php
+++ src/Generated/Model/NewOrderItem.php
@@ -25,7 +25,7 @@ class NewOrderItem
     /**
      * 
      *
-     * @var NewOrderItemPrice|null
+     * @var OrderItemPrice|null
      */
     protected $price;
     /**
@@ -34,6 +34,32 @@ class NewOrderItem
      * @var mixed|null
      */
     protected $options;
+    /**
+    * The price value rounded to 2 decimals with a dot used as separator. Note: This price value refers to
+    a single unit and is not an aggregated price value, which may be calculated by multiplying this price value by the
+    corresponding item count.
+    *
+    * @var float
+    */
+    protected $value;
+    /**
+     * The price type
+     *
+     * @var string
+     */
+    protected $type;
+    /**
+     * The VAT in percent
+     *
+     * @var float
+     */
+    protected $vat;
+    /**
+     * The currency code (ISO 4217)
+     *
+     * @var string
+     */
+    protected $currencyCode;
     /**
      * 
      *
@@ -100,20 +126,20 @@ class NewOrderItem
     /**
      * 
      *
-     * @return NewOrderItemPrice|null
+     * @return OrderItemPrice|null
      */
-    public function getPrice() : ?NewOrderItemPrice
+    public function getPrice() : ?OrderItemPrice
     {
         return $this->price;
     }
     /**
      * 
      *
-     * @param NewOrderItemPrice|null $price
+     * @param OrderItemPrice|null $price
      *
      * @return self
      */
-    public function setPrice(?NewOrderItemPrice $price) : self
+    public function setPrice(?OrderItemPrice $price) : self
     {
         $this->price = $price;
         return $this;
@@ -137,6 +163,94 @@ class NewOrderItem
     public function setOptions($options) : self
     {
         $this->options = $options;
+        return $this;
+    }
+    /**
+    * The price value rounded to 2 decimals with a dot used as separator. Note: This price value refers to
+    a single unit and is not an aggregated price value, which may be calculated by multiplying this price value by the
+    corresponding item count.
+    *
+    * @return float
+    */
+    public function getValue() : float
+    {
+        return $this->value;
+    }
+    /**
+    * The price value rounded to 2 decimals with a dot used as separator. Note: This price value refers to
+    a single unit and is not an aggregated price value, which may be calculated by multiplying this price value by the
+    corresponding item count.
+    *
+    * @param float $value
+    *
+    * @return self
+    */
+    public function setValue(float $value) : self
+    {
+        $this->value = $value;
+        return $this;
+    }
+    /**
+     * The price type
+     *
+     * @return string
+     */
+    public function getType() : string
+    {
+        return $this->type;
+    }
+    /**
+     * The price type
+     *
+     * @param string $type
+     *
+     * @return self
+     */
+    public function setType(string $type) : self
+    {
+        $this->type = $type;
+        return $this;
+    }
+    /**
+     * The VAT in percent
+     *
+     * @return float
+     */
+    public function getVat() : float
+    {
+        return $this->vat;
+    }
+    /**
+     * The VAT in percent
+     *
+     * @param float $vat
+     *
+     * @return self
+     */
+    public function setVat(float $vat) : self
+    {
+        $this->vat = $vat;
+        return $this;
+    }
+    /**
+     * The currency code (ISO 4217)
+     *
+     * @return string
+     */
+    public function getCurrencyCode() : string
+    {
+        return $this->currencyCode;
+    }
+    /**
+     * The currency code (ISO 4217)
+     *
+     * @param string $currencyCode
+     *
+     * @return self
+     */
+    public function setCurrencyCode(string $currencyCode) : self
+    {
+        $this->currencyCode = $currencyCode;
         return $this;
     }
 }
