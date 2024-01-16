a32a6099f4b2a0fefa2d7365b041457bb4512319
Datenkraft <office@datenkraft.com>
2024-01-16 14:22:47 +0000

    Autogenerated client update [publish]

--- src/Generated/Exception/PostOrderUnprocessableEntityException.php
+++ src/Generated/Exception/PostOrderUnprocessableEntityException.php
@@ -18,7 +18,6 @@ class PostOrderUnprocessableEntityException extends UnprocessableEntityException
 
 Error codes:
 - PRODUCT_NOT_FOUND: Unknown productNumber.
-- DUPLICATED_PRODUCT: There are multiple orderItems with the same productNumber.
 - ORDER_CUSTOMS_CLEARANCE_REQUIRED_FIELD_MISSING: A field required for customs clearance is missing.
 - ORDER_NUMBER_STARTS_WITH_RESERVED_NUMBER_PREFIX: The orderNumber starts with a prefix that is reserved for internal references.
 - ORDER_INVALID_CURRENCY_CODE: An invalid currencyCode was given for the delivery country.
