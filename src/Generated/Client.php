b7a240545913e279a1bf94c1fa0d5138e06afdcc
Datenkraft <office@datenkraft.com>
2026-09-14 11:02:09 +0000

    Autogenerated client update [publish]

--- src/Generated/Client.php
+++ src/Generated/Client.php
@@ -1411,6 +1411,7 @@ class Client extends \Datenkraft\Backbone\Client\FulfillmentApi\Generated\Runtim
     - damaged: Taking a damaged product off the books
     - own_withdrawal: Product taken for own use
     - correction: Manual correction
+    - inventory: Correction resulting from a completed inventory (stocktaking)
     - niceshops_order: Product sold via a shop from niceshops
     - inbound: Restocking the product
     - fulfillment: steve fulfilled an order
@@ -1503,6 +1504,7 @@ class Client extends \Datenkraft\Backbone\Client\FulfillmentApi\Generated\Runtim
     - damaged: Taking a damaged product off the books
     - own_withdrawal: Product taken for own use
     - correction: Manual correction
+    - inventory: Correction resulting from a completed inventory (stocktaking)
     - niceshops_order: Product sold via a shop from niceshops
     - inbound: Restocking the product
     - fulfillment: steve fulfilled an order
